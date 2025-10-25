cbba0c81e68de66569ccf95546f21bbd172f2cdb
MaluseLek <maluselekalakala@gmail.com>
2025-10-25 11:50:19 +0200

    Add Task 2 file to repo.

--- task_2.sql
+++ task_2.sql
@@ -1,35 +1,36 @@
-CREATE TABLE IF NOT EXISTS books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id VARCHAR(13) PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
     published_date DATE,
-    FOREIGN KEY (author_id) REFERENCES authors(name);
+    FOREIGN KEY (author_id) REFERENCES Authors(name);
 );
 
-CREATE TABLE IF NOT EXISTS authors (
+CREATE TABLE IF NOT EXISTS Authors (
     author_id VARCHAR(13) PRIMARY KEY,
-    name VARCHAR(215) NOT NULL
+    author_name VARCHAR(215) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) UNIQUE NOT NULL,
     address TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT PRIMARY KEY,
     customer_id INT NOT NULL,
     order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE IF NOT EXISTS order_details (
-    order_detail_id INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Order_Details (
+    orderdetailid INT PRIMARY KEY,
     order_id INT NOT NULL,
-    book_id VARCHAR(13) NOT NULL,
-    quantity INT NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    book_id INT NOT NULL,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
